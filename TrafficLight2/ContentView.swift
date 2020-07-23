81adfb6ee3c307866a04ff8aaa9dac867cf2ff0d
Гордей Гусельников <guselnikovgordey@icloud.com>
2020-07-23 12:21:57 +0700

    Initial Commit

---
+++ TrafficLight2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLight2
+//
+//  Created by Гордей Гусельников on 23.07.2020.
+//  Copyright © 2020 gordeyStudio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
